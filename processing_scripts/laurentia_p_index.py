e58d186fdb206f7ab85fb136b1c0bf13b30886bf
John J Czaplewski <jczaplew@gmail.com>
2016-06-27 10:46:05 -0500

    Add scripts for laurentia perimeter index

---
+++ processing_scripts/laurentia_p_index.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+conn = psycopg2.connect(dbname="alice", user="john", host="localhost", port=5432)
+cur = conn.cursor()
+
+cur.execute("""
+    DROP TABLE IF EXISTS laurentia_p_index;
+
+    CREATE TABLE laurentia_p_index (
+        year integer,
+        l_length numeric,
+        l_sublength numeric
+    );
+""")
+conn.commit()
+
+for year in xrange(0, 551):
+    cur.execute("""
+        INSERT INTO laurentia_p_index (year, l_length, l_sublength)
+        WITH notLaurentia AS (
+          SELECT ST_Union(geom) geom
+          FROM merge.reconstructed_""" + str(year) + """_merged
+          WHERE plateid != 101
+        ),
+        laurentia AS (
+          SELECT ST_Union(geom) geom
+          FROM (
+            SELECT ST_ExteriorRing((ST_Dump(geom)).geom) geom
+            FROM merge.reconstructed_""" + str(year) + """_merged
+            WHERE plateid = 101
+          ) sub
+        )
+        SELECT """ + str(year) + """ AS year, ST_Length(l.geom::geography) AS l_length, ST_Length(ST_Difference(l.geom, nl.geom)::geography) l_sublength
+        FROM laurentia l, notLaurentia nl
+    """)
+    print year
+
+conn.commit()
